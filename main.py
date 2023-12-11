a3fb47d59130ed4f624cd54f0ed01e00a13dcaff
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-11 19:24:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pickle
+#створення об'єкту для серефлізації
+data = {"name": "Valera", "age": 30, "city": "New York"}
+#сереалізація об'єкта в байтовий потік
+serialized_data = pickle.dumps(data) #перегляд
+print(serialized_data)
+#через контекстний менеджер
+with open("serialized_data.pickle", "wb") as file:
+    pickle.dump(data, file)
+#десеріалізація
+deserialized_data = pickle.loads(serialized_data)
+print(deserialized_data)
+#через контекстний менеджер
+with open("serialized_data.pickle", "rb") as file:
+    deserialized_data_file = pickle.load(file)
+print(deserialized_data_file)
