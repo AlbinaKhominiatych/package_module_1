16a703d5e19f23a3270e0285a58da8492d94571e
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-11 19:50:39 +0200

    zipfile

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-#приклад сиснення інформації
-import gzip
-with gzip.open('compressed_file.gz', "wb") as file:
-    file.write(b"some text")
-#ще один із видів стискання
-data = b"some text"
-compressed_data = gzip.compress(data)
-print(compressed_data)
-#розпакування стиснених данних
-decompressed_data = gzip.decompress(compressed_data)
-print(decompressed_data)
-#розпакування і файлу
-with gzip.open('compressed_file.gz', "rb") as file:
-    new_data = file.read()
-print(new_data)
+from zipfile import ZipFile
+
+with ZipFile("metanit.zip", "a") as myzip:
+    myzip.write("eggs.txt")
+    myzip.write("serialized_data.pickle")
